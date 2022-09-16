f701b39e15159621262c3799b0cdd47c874cf69f
Bharat-Fexle-May2022 <105696774+Bharat-Fexle-May2022@users.noreply.github.com>
2022-09-16 18:12:51 +0530

    Modified

--- src/lwc/tileViewLwc/tileViewLwc.js
+++ src/lwc/tileViewLwc/tileViewLwc.js
@@ -4,17 +4,13 @@ export default class tileViewLwc extends LightningElement {
     contacts;
     error;
     connectedCallback() {
-        debugger;
 		this.loadContact();
 	}
 	loadContact() {
-		getContactTile()
-			.then(result => {
-				this.contacts = result;
-			})
-			.catch(error => {
-				this.error = error;
-			});
+	    getContactTile()
+		.then(result => {
+			this.contacts = result;
+		});
 	}
 
 }
